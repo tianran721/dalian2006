f311c1c92d5cc9b1fd9b31bfd5f18402da361df2
tianran7211 <858448643@qq.com>
2020-11-12 19:21:24 +0800

    轮播ok

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-
-  lintOnSave: false, //关闭eslint
+  // lintOnSave: false, //关闭eslint
   devServer: {
+    overlay: {
+      //忽略遮罩层
+      warnings: false,
+      errors: false
+    },
     proxy: {
-      v2: {
+      '/v2': {
         target: 'https://m.wowdsgn.com',
         changeOrigin: true
       }
