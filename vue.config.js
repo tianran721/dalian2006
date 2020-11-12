ed45808885c25b2f4d2a1b48a1a0d52eccc4b448
zzz <1277709592@qq.com>
2020-11-12 19:27:40 +0800

    add class

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   lintOnSave: false, //关闭eslint
   devServer: {
     proxy: {
-      v2: {
+      '/v2': {
         target: 'https://m.wowdsgn.com',
         changeOrigin: true
       }
